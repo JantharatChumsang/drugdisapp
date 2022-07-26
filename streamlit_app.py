849177ac69b97815727d92790262b1ff3e7f6f88
Jantharat Chumsang <jantharatchjune@gmail.com>
2022-07-26 16:50:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,10 +45,10 @@ from rdkit.Chem import AllChem
 from rdkit import Chem
 from rdkit.Avalon import pyAvalonTools
 from rdkit import Chem, DataStructs
-# from rdkit.Chem.Draw import SimilarityMaps, IPythonConsole
-# from rdkit.Chem.Draw import rdMolDraw2D
+from rdkit.Chem.Draw import SimilarityMaps, IPythonConsole
+from rdkit.Chem.Draw import rdMolDraw2D,ShowMol
 from chembl_webresource_client.new_client import new_client
-# from pikachu.general import draw_smiles
+from pikachu.general import draw_smiles
 from rdkit import rdBase
 rdBase.DisableLog('rdApp.error')
 
